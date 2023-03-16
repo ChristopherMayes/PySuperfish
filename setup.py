26a4a198336585a8e23f10cd7f03fbf5e53e1d27
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2023-03-16 09:37:54 -0700

    Add versioneer

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 from os import path, environ
 
+import versioneer
+
+
 cur_dir = path.abspath(path.dirname(__file__))
 
 with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
@@ -10,13 +13,14 @@ with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
 
 setup(
     name='pySuperfish',
-    version = '0.0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),  
     packages=find_packages(),  
     package_dir={'superfish':'superfish'},
-    url='https://github.com/ChristopherMayes/pySuperfish',
+    url='https://github.com/ChristopherMayes/PySuperfish',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=requirements,
     include_package_data=True,
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
--- superfish/__init__.py
+++ superfish/__init__.py
@@ -1,3 +1,3 @@
-
-
 from .superfish import Superfish
+from . import _version
+__version__ = _version.get_versions()['version']
